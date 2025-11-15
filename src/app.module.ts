655a370a7b4e8ce5696d03c3bfdeb7e9f4e50c38
Na1gae <nalgae@korea.ac.kr>
2025-11-15 21:39:38 +0900

    feat: Set up Swagger documentation and validation pipes

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,19 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaService } from './prisma/prisma.service';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { FirebaseModule } from './firebase.module';
+import { ProjectModule } from './project/project.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true })],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }), 
+    AuthModule, 
+    UserModule,
+    FirebaseModule,
+    ProjectModule
+  ],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Team4 API')
+    .setDescription('Team4 API description')
+    .setVersion('0.1')
+    .addBearerAuth()
+    .build();
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+    }),
+  );
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
